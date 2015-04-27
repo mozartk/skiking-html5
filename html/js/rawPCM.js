a1c7603d81ffe315fe75e92ba71d2e9a4d3caf78
mozartk <mozartk30@gmail.com>
2015-04-27 22:40:23 +0900

    add config, file slice method

--- html/js/rawPCM.js
+++ html/js/rawPCM.js
@@ -2,35 +2,51 @@ define([],
   function(){
     'use strict';
 
+    var HeaderObj = {
+      "sampleRate" : 10800,
+      "channel": 1,
+      "bps": 8
+    };
+
     function rawpcm(){
+      if(typeof arguments[0] === "object" && arguments.length === 1){
+        console.log(arguments[0]);
+        for(var key in arguments[0]){
+          HeaderObj[key] = arguments[0][key];
+        }
+      } else if(arguments > 0){
+        HeaderObj["sampleRate"] = arguments[0];
+        if(typeof arguments[1] !== "undefined") HeaderObj["channel"] = arguments[1];
+        if(typeof arguments[2] !== "undefined") HeaderObj["bps"] = arguments[2];
+      }
     };
 
     rawpcm.prototype.makeHeader = function(data){
       // http://www-mmsp.ece.mcgill.ca/Documents/AudioFormats/WAVE/WAVE.html
 
-      var sampleRate = 10800;
-      var channel = 1;
-      var bps = 8;
+      var sampleRate = HeaderObj["sampleRate"];
+      var channel    = HeaderObj["channel"];
+      var bps        = HeaderObj["bps"];
 
-      var blockAlign = (channel * bps) >> 3;
-      var byteRate = blockAlign * sampleRate;
+      var blockAlign    = (channel * bps) >> 3;
+      var byteRate      = blockAlign * sampleRate;
       var subChunk2Size = data.length * (bps >> 3);
-      var chunkSize = 36 + subChunk2Size;
-      
+      var chunkSize     = 36 + subChunk2Size;
+
       var t = this;
       var arr = [];
-      arr = arr.concat(t.g("RIFF"), 
-        t.g(chunkSize, 4), 
-        t.g("WAVE"), 
-        t.g("fmt "), 
-        t.g(16, 4), 
-        t.g(1, 2), 
-        t.g(1, 2), 
+      arr = arr.concat(t.g("RIFF"),
+        t.g(chunkSize, 4),
+        t.g("WAVE"),
+        t.g("fmt "),
+        t.g(16, 4),
+        t.g(1, 2),
+        t.g(1, 2),
         t.g(sampleRate, 4),
         t.g(byteRate, 4),  //byteRate
         t.g(blockAlign, 2),  //blockAlign
-        t.g(8, 2), 
-        t.g("data"), 
+        t.g(8, 2),
+        t.g("data"),
         t.g(subChunk2Size, 4)
       );
 
@@ -94,18 +110,51 @@ define([],
       return arr;
     };
 
-    rawpcm.prototype.getWav = function(data){
-      var dataLen = data.length;
-      var header = this.makeHeader(data);
+
+    //return header + body
+    rawpcm.prototype.concat = function(header, body){
       var headerLen = header.length;
+      var dataLen = body.length;
 
       var dataWav = new Uint8Array(headerLen + dataLen);
       dataWav.set(header);
-      dataWav.set(data, headerLen);
+      dataWav.set(body, headerLen);
+
+      return dataWav;
+    };
+
+    //return header + body
+    rawpcm.prototype.subarray = function(dataWav, startIdx, size){
+      if(typeof size === "number"){
+        dataWav = new Uint8Array(dataWav.subarray(startIdx, size));
+      } else {
+        dataWav = new Uint8Array(dataWav.subarray(startIdx));
+      }
+
+      return dataWav;
+    };
+
+
+    //getWav
+    rawpcm.prototype.getWav = function(data, startIdx, size){
+      var dataWav;
+
+      if(data instanceof ArrayBuffer === true) {
+        dataWav = new Uint8Array(data);
+      } else {
+        dataWav = data;
+      }
+
+      if(typeof startIdx === "number"){
+        dataWav = this.subarray(dataWav, startIdx, size);
+      }
+
+      var header = this.makeHeader(dataWav);
+      dataWav = this.concat(header, dataWav);
 
       return dataWav;
     }
 
     var rawPCM = rawpcm;
-    return new rawPCM;
+    return rawPCM;
   });
