0cc9fe09c51e38cf1081bbcffc6c1910d5f2dd6a
mozartk <mozartk30@gmail.com>
2015-04-27 23:07:36 +0900

    soundFx init

---
+++ html/js/soundFx.js
@@ -0,0 +1,30 @@
+define(["rawPCM"],
+  function(rawPCM){
+    'use strict';
+
+    function soundFx(sound){
+      this.sound = sound;
+      this.soundLength = sound.length;
+      this.cutPoint = [
+        ["clap", 30000],
+        ["oops", 60000],
+        ["oops2", this.soundLength]
+      ];
+      this.soundObj = {};
+
+      var rawpcm = new rawPCM();
+
+      var idx = 0;
+      for(var arrIdx in this.cutPoint){
+        var array = this.cutPoint[arrIdx];
+        this.soundObj[array[0]] = rawpcm.getWav(this.sound, idx, (array[1]-idx));
+        idx = array[1];
+      }
+    };
+
+    soundFx.prototype.get = function(key){
+      return this.soundObj[key];
+    }
+
+    return soundFx;
+  })
