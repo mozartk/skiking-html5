8a0eb91d6d8f404f8d324685128c0aa0885fe90e
mozartk <mozartk30@gmail.com>
2015-05-01 13:58:12 +0900

    add event control

--- html/js/asdfJSEngine.js
+++ html/js/asdfJSEngine.js
@@ -4,28 +4,20 @@ define(["jquery", "underscore"],  function($, _){
   var currentLayerLevel = 100;
   var storeLayer = [];
 
-  //default layer option
-  var layerOption = {
-    "keyEvent": ["keyup", "keydown", "keypress", "click"],
-    "visible": true
-  }
-
   function asdfJSEngine(){
-    eventInit();
-    this.addLayer(layerOption);
+    event.init();
+    this.addLayer(100, {});
+    this.addLayer(101, {visible: false, enabled: false, "layer":"_basic"});
   };
 
-  asdfJSEngine.prototype.addLayer = function(newLayerOption){
-    var layerOption = _.extend(layerOption, newLayerOption);
+  asdfJSEngine.prototype.addLayer = function(layerIdx, newLayerOption){
+    var layerOption = _.extend(defaultLayerOption(), newLayerOption, {layerLevel: layerIdx});
+    var that = this;
 
-    _addLayer(layerOption);
-  }
-
-  function _addLayer(layerOption){
-    require(["gameLayers/_basic"], function(basicLayer){
-      storeLayer[currentLayerLevel] = new basicLayer(layerOption);
+    require(["gameLayers/" + layerOption.layer], function(basicLayer){
+      storeLayer[layerIdx] = new basicLayer(layerOption, that);
     });
-  }
+  };
 
   function setVisible(visible){
     console.log(visible);
@@ -41,7 +33,7 @@ define(["jquery", "underscore"],  function($, _){
 
   function getLayer(){
     var layerIdx;
-    if(typeof arguments[0] === "number"){
+    if(typeof parseInt(arguments[0]) === "number"){
       layerIdx = arguments[0];
     } else {
       layerIdx = currentLayerLevel;
@@ -50,34 +42,62 @@ define(["jquery", "underscore"],  function($, _){
     return storeLayer[layerIdx];
   }
 
-  function defalutEventList(){
-    return layerOption.keyEvent.join(" ");
-  }
 
-  function eventInit(){
-    $("#gameDiv").on(defalutEventList(), eventDistribute);
-  }
 
-  //이벤트를 윗 레이어에서 아래 레이어로 분배함
-  //true면 분배 중단함
-  function eventDistribute(){
-
-    //배열을 거꾸로 돌려서 체크해야 하는데 for in은 거꾸로 못돌림... 그래서 한번 더 돌려서 체크함;;
-    var idxArr = [];
-    for(var idx in storeLayer){
-      idxArr.push(~~idx);
+  //default layer option
+  function defaultLayerOption(newOpt){
+    var opt =  {
+      "keyEvent": ["keyup", "keydown", "keypress", "click"],
+      "layer": "_basic",
+      "enabled": true,
+      "visible": true
     }
 
-    //높은 순서부터 이벤트 체크함
-    var len = idxArr.length;
-    while(len){
-      var result = getLayer(idxArr[len-1]).event(arguments[0]);
-
-      if(result) break; //true이면 이벤트 전파를 종료함
-
-      len--;
+    if(typeof newOpt !== "undefined"){
+      return opt;
+    } else {
+      return _.extend(opt, newOpt);
     }
+  }
 
+
+  //event Controll part
+  var event = {
+    init: function(){
+      $("#gameDiv").on(event.list(), event.distribute);
+    },
+
+    //각 레이어에 필요한 이벤트 정의함
+    list: function(){
+      var keyEvent = defaultLayerOption().keyEvent;
+      if(typeof arguments[0] !== 'undefined'){
+        keyEvent = arguments[0];
+      }
+      return keyEvent.join(" ");
+    },
+
+    //이벤트 각 레이어로 분배함
+    distribute: function(){
+      //배열을 거꾸로 돌려서 체크해야 하는데 for in은 거꾸로 못돌림... 그래서 한번 더 돌려서 체크함;;
+      var idxArr = [];
+      for(var idx in storeLayer){
+        if(getLayer(idx).layerOption.enabled === false) {
+          continue;
+        }
+
+        idxArr.push(~~idx);
+      }
+
+      //높은 순서부터 이벤트 체크함
+      var len = idxArr.length;
+      while(len){
+        var result = getLayer(idxArr[len-1]).event(arguments[0]);
+
+        if(result) break; //true이면 이벤트 전파를 종료함
+
+        len--;
+      }
+    }
   }
 
   return asdfJSEngine;
