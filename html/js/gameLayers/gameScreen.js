59886c70570c8f4df11184b79a287f6d742c0be8
mozartk <mozartk30@gmail.com>
2015-05-05 18:28:01 +0900

    활강 구

--- html/js/gameLayers/gameScreen.js
+++ html/js/gameLayers/gameScreen.js
@@ -2,13 +2,15 @@
 define(["jquery", "underscore", "stageMaker"],  function($, _, StageMaker){
   'use strict';
 
-  var gameScreen;
   var reDraw = false;
   var soundFx;
 
-  var stage, skiTile;
+  var stage, stageLen, skiTile;
+  var gameSpeed = 3;
+  var gameTick = 5;
 
   var player = {
+    run: false, //run state
     skisel: 0, //skisel
     skiselDirect:1, // 0left 1mid 2right
     currentPosX:16, //0~31
@@ -23,6 +25,14 @@ define(["jquery", "underscore", "stageMaker"],  function($, _, StageMaker){
   var bufferCtx  = scrBuffer.getContext('2d');
   bufferCtx.imageSmoothingEnabled = false;
 
+  //미리 준비해 놓는 합성 이미지
+  var compImg = document.createElement('canvas');
+  compImg.width = engine.screenConf.rw;
+  compImg.height = engine.screenConf.rh;
+
+  var compCtx  = compImg.getContext('2d');
+  compCtx.imageSmoothingEnabled = false;
+
   var gameSprites = {
     'snow':[0,1,2,3,8,9,16,17,24,25],
     'snowTrail':[0,1,2,3,8,9,16,17,24,25],
@@ -30,7 +40,7 @@ define(["jquery", "underscore", "stageMaker"],  function($, _, StageMaker){
     'sky':[52],
     'horizon':[60, 61],
     'endLine': [38,39] //두개를 위아래로 겹쳐서 4줄로 그려야 함(명암, 실제로는 2줄)
-  }
+  };
 
 
   window.maker = StageMaker;
@@ -38,7 +48,6 @@ define(["jquery", "underscore", "stageMaker"],  function($, _, StageMaker){
   function gameScreenLayer(layerOption, engine){
     this.layerOption = layerOption;
     this.engine = engine;
-    this.bufferCtx = bufferCtx;
     soundFx = engine.soundFx;
     skiTile  = engine.gameImage.getImage('ski');
 
@@ -47,28 +56,55 @@ define(["jquery", "underscore", "stageMaker"],  function($, _, StageMaker){
 
   function init(){
     makeStage();
+    makeCompImage();
+    playerInit();
     drawGameScreen();
   }
 
   function drawGameScreen(){
     reDraw = true;
+    engine.painter.redraw();
   }
 
   function makeStage(){
-    var skiTile  = engine.gameImage.getImage('ski');
-    var i = 0;
-
     var stageMaker = new StageMaker();
     stage = stageMaker.seed(Date.now()).get(1);
-
-    //sky
-
+    stageLen = stage.length;
 
     window.stage = stage;
   }
 
-  function playerSettings(){
+  function playerInit(){
+    player = {
+      skisel: 0, //skisel
+      skiselDirection:1, // 0left 1mid 2right
+      currentPosX:16, //0~31
+      currentPosY:0, //scroll pos
+      speedState: 0, //0~1
+      distance: 0, //0~~
+      dead: 0 //0alive 1dead
+    };
+  }
 
+  function makeCompImage(){
+    //결승선
+    compCtx.drawImage(skiTile, 145, 46, 10, 2, 5, 5, 10, 1);
+    compCtx.drawImage(skiTile, 125, 47, 10, 2, 5, 6, 10, 1);
+    compCtx.drawImage(skiTile, 145, 46, 10, 2, 5, 7, 10, 1);
+    compCtx.drawImage(skiTile, 125, 47, 10, 2, 5, 8, 10, 1);
+
+    //시작시점
+    for(var i=10; i<=40; i=i+10){
+      compCtx.drawImage(skiTile, 5, 85, 10, 5, i, 10, 10, 5);
+      compCtx.drawImage(skiTile, 25, 85, 10, 5, i, 15, 10, 5);
+      compCtx.drawImage(skiTile, 125, 75, 10, 1, i, 20, 10, 1);
+      compCtx.drawImage(skiTile, 125, 76, 10, 4, i, 21, 10, 9);
+      compCtx.drawImage(skiTile, 145, 75, 10, 9, i, 29, 10, 1);
+    }
+
+    //player
+    compCtx.drawImage(skiTile, 9, 100,  2, 1,  12, 35, 2, 1);
+    compCtx.drawImage(skiTile, 27, 100, 6, 10, 10, 36, 6, 19);
   }
 
   function paintStage(){
@@ -80,8 +116,8 @@ define(["jquery", "underscore", "stageMaker"],  function($, _, StageMaker){
     for(i=player.currentPosY; i<=len; i++){
       var v = stage[i];
       v.forEach(function(vv, kk){
-        if(vv>=0 && vv<=9 || vv == 20) {
-          if(vv === 20) vv = 0;
+        if(vv>=0 && vv<=9 || vv == 20 || vv == 34) {
+          if(vv === 20 || vv == 34) vv = 0;
           var row = Math.floor(tile[vv]/8);
           var column = ((tile[vv]/8) - row)*8;
           var r_row = row * 10;
@@ -92,42 +128,107 @@ define(["jquery", "underscore", "stageMaker"],  function($, _, StageMaker){
       });
       k++;
     };
-  }
+  };
 
   function paintMaterial(ctx){
-    var k = 0;
-    var i = player.currentPosY;
+    var k = 0, i, j = 0;
     var len = player.currentPosY+50;
-    var tile = gameSprites;
     for(i=player.currentPosY; i<=len; i++){
       var v = stage[i];
       v.forEach(function(vv, kk){
-        //tree
         if(vv == 20){
+          //tree
           bufferCtx.drawImage(skiTile, 104, 10, 11, 10, kk*10, (k*5)-14, 11, 19);
         } else if(vv >= 30 && vv <= 33){
-          var _tile;
-          switch(vv){
-            case 30:
-            case 31:
-              _tile = tile.sky[0];
-              break;
-            case 32:
-            case 33:
-              _tile = tile.horizon[vv-32];
-              break;
-          }
+          //sky, horizon
+          var _tile = getTile(vv);
+
           var row = Math.floor(_tile/8);
           var column = ((_tile/8) - row)*8;
           var r_row = row * 10;
           var r_column = column * 20;
           bufferCtx.drawImage(skiTile, r_column+5, r_row+5, 10, 5, kk* 10, k*5, 10, 10);
+        } else if(vv == 34){
+          //finish line
+          bufferCtx.drawImage(compImg, 5, 5, 10, 4, kk* 10, k*5, 10, 4);
+        } else if(vv == 36){
+          if(j==0) {
+            bufferCtx.drawImage(compImg, 10, 10, 40, 20, kk*10, k*5, 40, 20);
+            j++;
+          }
         }
-
       });
       k++;
     };
   }
+
+  function playerDirection(){
+    var spritePos = {};
+    switch(player.skisel){
+      case 0:
+        spritePos = {
+          x: 10,
+          y: 35,
+          w: 6,
+          h: 20,
+          cx: 160,
+          cy: 125,
+          rw: 6,
+          rh: 20
+        }
+        break;
+    }
+
+    return spritePos;
+  }
+
+  function paintPlayer(){
+    var p = playerDirection();
+    bufferCtx.drawImage(compImg, p.x, p.y, p.w, p.h, p.cx, p.cy, p.rw, p.rh);
+  }
+
+  //처리 후 한칸 전진
+  function playerFF(){
+    //앞에 장애물이 있는지 확인
+
+    //없을 경우
+    if(false){
+      player.distance++;
+      if(player.distance%4 <= 1){
+        player.state = 1;
+      } else {
+        player.state = 0;
+      }
+    } else {
+      player.dead = true;
+    }
+
+    player.currentPosY += 2;
+  }
+
+  function getTile(resourceId){
+    switch(resourceId){
+      case 30:
+      case 31:
+        return gameSprites.sky[0];
+        break;
+      case 32:
+      case 33:
+        return gameSprites.horizon[resourceId-32];
+        break;
+    }
+  }
+
+
+
+  function startRun(){
+    player.run = true;
+  }
+
+  function finishRun(){
+    player.run = true;
+  }
+
 
   //event 처리 부분
   //true를 리턴하면 키를 여기서 먹도록 처리
@@ -135,19 +236,26 @@ define(["jquery", "underscore", "stageMaker"],  function($, _, StageMaker){
   gameScreenLayer.prototype.event = function(e){
     if(e.type == 'keydown') {
       switch (e.keyCode) {
-        case 40:
-          if(player.currentPosY <=90) {
-            player.currentPosY += 2;
-          }
-          break;
-        case 38:
-          if(player.currentPosY >=2) {
-            player.currentPosY -= 2;
-          }
-          break;
+        //case 40:
+        //  if(player.currentPosY <=stageLen) {
+        //    player.currentPosY += 2;
+        //  }
+        //  break;
+        //case 38:
+        //  if(player.currentPosY >=2) {
+        //    player.currentPosY -= 2;
+        //  }
+        //  break;
       }
-      reDraw = true;
+
+      if(player.run !== true){
+        startRun();
+        drawGameScreen();
+      }
+
+
     }
+
     return true;
   };
 
@@ -155,17 +263,26 @@ define(["jquery", "underscore", "stageMaker"],  function($, _, StageMaker){
     if(reDraw === true){
       paintStage();
       paintMaterial();
+      paintPlayer();
 
       ctx.drawImage(scrBuffer, 0, 0);
     }
 
-    reDraw = false;
+    drawGameScreen();
+
+    if(player.run === true) {
+      gameTick++;
+      if (gameTick % gameSpeed === 0) {
+        playerFF();
+      }
+
+
+    } else {
+      reDraw = false;
+    }
+
+
   }
-
-
-
-
-
 
   return gameScreenLayer;
 });
